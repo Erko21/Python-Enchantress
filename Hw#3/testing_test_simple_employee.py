587a5ab35c7051ce7551a1b0e794acd319d996ae
Erko21 <ekedesh@gmail.com>
2021-01-25 21:00:34 +0200

    Homework #3

--- Hw#3/testing_test_simple_employee.py
+++ Hw#3/testing_test_simple_employee.py
@@ -3,30 +3,31 @@ from unittest import mock
 import test_simple_employee
 import utils
 
-Employer = test_simple_employee.Employee('Ernest', 'M', 2000)
-
 
 class EmployeeTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.Employer = test_simple_employee.Employee('Ernest', 'M', 2000)
+
     def test_email(self):
-        self.assertEqual(Employer.email, 'ekedesh@gmail.com')
+        self.assertEqual(self.Employer.email, 'ekedesh@gmail.com')
 
     def test_full_name(self):
-        self.assertEqual(Employer.fullname, 'Ernest M')
+        self.assertEqual(self.Employer.fullname, 'Ernest M')
 
     def test_employee_info(self):
-        self.assertEqual(Employer.first, 'Ernest')
-        self.assertEqual(Employer.last, 'M')
-        self.assertEqual(Employer.pay, 2000)
+        self.assertEqual(self.Employer.first, 'Ernest')
+        self.assertEqual(self.Employer.last, 'M')
+        self.assertEqual(self.Employer.pay, 2000)
 
     @mock.patch("utils.make_request", return_value=True)
     def test_monthly_schedule(self, month):
-        response = utils.parse_response(Employer.last, 3)
+        response = utils.parse_response(self.Employer.last, 3)
         self.assertEqual(response, "Successful request")
 
     def test_raise(self):
-        Employer.apply_raise()
-        self.assertEqual(Employer.pay, 2100)
+        self.Employer.apply_raise()
+        self.assertEqual(self.Employer.pay, 2100)
 
 
 if __name__ == "__main__":
